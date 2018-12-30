372531571dce314ffc3a993d52a8dd1db49321c0
ATC <atcsecure@users.noreply.github.com>
2018-12-30 11:42:21 -0500

    Update dxsettings.py

--- utils/dxsettings.py
+++ utils/dxsettings.py
@@ -1,6 +1,7 @@
 tradingaddress = {}
 tradingaddress['SYS'] = "_address_here_"
 tradingaddress['LTC'] = "_address_here_"
+tradingaddress['BLOCK'] = "_address_here_"
 rpcport = 41414
 rpcuser = '<rpcuser>'
 rpcpassword = '<rpcpass>'
