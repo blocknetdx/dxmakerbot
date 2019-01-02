3c873df77d1b12a0ab3169a07b58dc7dc6c74ef3
atcsecure <dmetcalf@atcsecure.com>
2019-01-02 21:47:42 +0000

    refactoring/cleanup

--- dxmakerbot.py
+++ dxmakerbot.py
@@ -11,18 +11,13 @@ from utils import dxsettings
 parser = argparse.ArgumentParser()
 parser.add_argument('--maker', help='maker chain', default='BLOCK')
 parser.add_argument('--taker', help='taker chain', default='LTC')
-parser.add_argument('--base', help='base chain for pricing')
 parser.add_argument('--slidemin', help='price slide adjustment', default=0.999887)
 parser.add_argument('--slidemax', help='price slide adjustment', default=1.015999)
-parser.add_argument('--cancelall', help='cancel all orders and exist')
+parser.add_argument('--cancelall', help='cancel all orders and exist', type=bool)
 parser.add_argument('--sellmin', help='maker sell min order size', default=0.001)
 parser.add_argument('--sellmax', help='maker sell max order size', default=1)
 args = parser.parse_args()
 
-#
-print ('running with args')
-print (args)
-##
 BOTsellmarket = args.maker.upper()
 BOTbuymarket = args.taker.upper()
 BOTslidemin = float(args.slidemin)
@@ -32,20 +27,12 @@ if args.cancelall:
   results = dxbottools.cancelallorders()
   print (results)
   sys.exit(0)
-BOTbase = args.base.upper()
-
-if BOTbase == BOTsellmarket:
-  BOTtake = BOTbuymarket
-else:
-  BOTtake = BOTsellmarket
-
 
 time.sleep(1.5) # wait for cancel orders
-print ("start bot")
-print ('bottake: {0}'.format(BOTtake))
+print ('start bot')
 print (BOTsellmarket, BOTbuymarket)
-print (" - checking trex api ...")
-print ('makers market price: %s' %(trexbot.getpricedata(BOTbase, BOTtake)))
+print (' - checking trex api ...')
+print ('makers market price: %s' %(trexbot.getpricedata(BOTsellmarket, BOTbuymarket)))
 # init values
 maxloopcount = 30 # 1 loop per minute, then cancel all orders, start over
 loopcount = 0
@@ -68,7 +55,7 @@ if __name__ == "__main__":
     print('pre-start balances: %s' % blockbalance)
     while blockbalance > 0:
       print ('balance ok')
-      makermarketprice = trexbot.getpricedata(BOTbase, BOTtake)
+      makermarketprice = trexbot.getpricedata(BOTsellmarket, BOTbuymarket)
       print ('marketprice: {0}'.format(makermarketprice))
       print ('loopcount', loopcount)
       print ('ordercount', ordercount)
@@ -95,20 +82,16 @@ if __name__ == "__main__":
       # we have the price per block sellamount * makermarketprice
       print ('makerprice: {0}'.format(makermarketpriceslide))
 
-      # calc buyamount
-      if args.base == args.maker:
-        buyamount = float(sellamount) * float(makermarketpriceslide)
-      else:
-        buyamount = float(sellamount) / float(makermarketpriceslide)
+      print ('BOTsellmarket {0}'.format(BOTsellmarket))
+      print ('BOTbuymarket {0}'.format(BOTbuymarket))
+      print ('makerprice {0}'.format(makermarketpriceslide))
+      buyamount = (float(sellamount) * float(makermarketpriceslide)) 
       buyamountclean = '%.6f' % buyamount
       print ('buyamount {0}'.format(buyamountclean))
       currentopenorders = len(dxbottools.getopenorderIDs())
       if (ordercount < maxordercount) and (currentopenorders < (maxordercount*2)):
         results = dxbottools.rpc_connection.dxMakeOrder(BOTsellmarket, str(sellamount), makeraddress, BOTbuymarket, str(buyamountclean), takeraddress, "exact")
-        #print (results['id'])
-        #print (results['taker_size'])
-        #print (results['maker_size'])
-        print (results)
+        print ('{0} {1} {2}'.format(results['id'], results['taker_size'], results['maker_size']))
       else:
         print('too many orders open')
       loopcount += 1
@@ -124,7 +107,7 @@ if __name__ == "__main__":
       loopcount += 1
     if loopcount > maxloopcount:
       dxbottools.canceloldestorder()
-      print ("canceled oldest")
+      print ('canceled oldest')
       loopcount = 0
       ordercount = 0
       time.sleep(3.5)
