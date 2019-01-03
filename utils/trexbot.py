f0d66bffbbcbfa6d70241d2ea412dbc0f93c9137
atcsecure <dmetcalf@atcsecure.com>
2019-01-03 12:57:09 -0500

    updated to support BTC pairing

--- utils/trexbot.py
+++ utils/trexbot.py
@@ -15,6 +15,13 @@ my_bittrex = Bittrex(None, None)
 
 def getmarketprice(marketname):
   # get market price
+  markets = []
+  markets = marketname.split('-')
+  print (marketname)
+  if markets[1] == 'BTC':
+    print ('split')
+    marketname = '{0}-{1}'.format(markets[1], markets[0])
+    print (marketname)
   summary = my_bittrex.get_market_summary(marketname)
   for attempt in range(0,5):
       try:
@@ -31,14 +38,21 @@ def getmarketprice(marketname):
 def getpricedata(maker, taker):
   basemarket = ('BTC-{0}'.format(maker))
   takermarket = ('BTC-{0}'.format(taker))
-
+  print ('maker: {0}, taker: {1}'.format(maker,taker))
+  print ('basemarket: %s' % basemarket)
+  if maker == 'BTC':
+    marketprice = 1/getmarketprice(takermarket)
+    return marketprice
   makerprice = getmarketprice(basemarket)
-  takerprice = getmarketprice(takermarket)
-
-  try:
-    marketprice = makerprice / takerprice
-  except:
-    marketprice = 0
+  print ('takermarket: %s' % takermarket)
+  if taker == 'BTC':
+    marketprice = makerprice
+  else:
+    takerprice = getmarketprice(takermarket)
+    try:
+      marketprice = makerprice / takerprice
+    except:
+      marketprice = 0
 
   return marketprice
 
