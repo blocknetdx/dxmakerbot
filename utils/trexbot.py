c9e5b303ecd2d6433b0c1a709b8c373a23393492
atcsecure <dmetcalf@atcsecure.com>
2019-03-01 11:59:08 -0500

    add support for pricing from cryptobridge api

--- utils/trexbot.py
+++ utils/trexbot.py
@@ -9,9 +9,12 @@ __status__ = "Alpha"
 
 import json
 import time
+import requests
 from bittrex.bittrex import Bittrex, API_V2_0
 
 my_bittrex = Bittrex(None, None)
+#cryptobridgeURL = 'https://api.crypto-bridge.org/api/v1/ticker'
+cryptobridgeURL = ''  # set this value to use crypto bridge pricing
 
 def getmarketprice(marketname):
   # get market price
@@ -22,18 +25,38 @@ def getmarketprice(marketname):
     print ('split')
     marketname = '{0}-{1}'.format(markets[1], markets[0])
     print (marketname)
-  summary = my_bittrex.get_market_summary(marketname)
-  for attempt in range(0,5):
-      try:
-        lastprice = summary['result'][0]['Last'] 
-      except Exception as e:
-        print('API call attempt# {2} - exception/error {0}, bittrex called failed for {1}'.format(e, marketname, attempt))
-        print(summary)
-        time.sleep(2.5)
-        lastprice = 0
-        continue
-      break
-  return lastprice
+
+  if cryptobridgeURL:
+    print ('using cryptobridge for pricing')
+    resp = requests.get(url=cryptobridgeURL)
+    data = resp.json()
+    marketname = '{0}_{1}'.format(markets[1], markets[0])
+    print ('>>> {}'.format(marketname))
+    #print (data)
+    for z in data:
+      #print (z['id'])
+      if (z['id']) == marketname:
+        #print (z['last'])
+        #print (z)
+        lastprice = z['last']
+
+  else:
+    summary = my_bittrex.get_market_summary(marketname)
+
+    for attempt in range(0,5):
+        try:
+          lastprice = summary['result'][0]['Last'] 
+        except Exception as e:
+          print('API call attempt# {2} - exception/error {0}, bittrex called failed for {1}'.format(e, marketname, attempt))
+          print(summary)
+          time.sleep(2.5)
+          lastprice = 0
+          continue
+        break
+
+  #lastprice = '%.8f' % float(lastprice) 
+  print (lastprice)
+  return float(lastprice)
 
 def getpricedata(maker, taker):
   basemarket = ('BTC-{0}'.format(maker))
